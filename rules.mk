845e7d31ad87bbcabe84d3c7d2bc20a01a568317
Krister Svanlund <kofish@mailbox.org>
2023-10-09 21:24:06 +0200

    🎮 Added more of my config

--- rules.mk
+++ rules.mk
@@ -1,1 +1,5 @@
 SRC += source.c
+
+CONVERT_TO = liatris
+SWAP_HANDS_ENABLE = yes
+WPM_ENABLE = yes
--- source.c
+++ source.c
@@ -1,1 +1,360 @@
 #include QMK_KEYBOARD_H
+
+enum layers {
+    _BASE,
+    _EXT,
+    _FUN,
+    _SYM,
+    _NUM,
+    _FIVE,
+    _SIX,
+};
+
+#ifdef OLED_ENABLE
+#include <stdio.h>
+// NOTE: Most of the OLED code was originally written by Soundmonster for the Corne,
+// and has been copied directly from `crkbd/soundmonster/keymap.c`
+
+enum layer_indicator_oled {
+    _MID_1 = 0x95,
+    _MID_2 = 0xb5,
+    _MID_3 = 0xd5,
+    _MID_4 = 0xbc,
+    _TOP_EMPTY = 0x94,
+    _TOP_LEFT_EMPTY_1 = 0x93,
+    _TOP_RIGHT_EMPTY_1 = 0x97,
+    _TOP_LEFT_EMPTY_2 = 0xb3,
+    _TOP_RIGHT_EMPTY_2 = 0xb6,
+    _TOP_LEFT_EMPTY_3 = 0xb4,
+    _TOP_RIGHT_EMPTY_3 = 0xb7,
+    _TOP_LEFT_FULL_1 = 0x98,
+    _TOP_RIGHT_FULL_1 = 0x9a,
+    _TOP_LEFT_FULL_2 = 0xb8,
+    _TOP_RIGHT_FULL_2 = 0xba,
+    _TOP_LEFT_FULL_3 = 0x99,
+    _TOP_RIGHT_FULL_3 = 0x9b,
+    _TOP_LEFT_FULL_4 = 0xb9,
+    _TOP_RIGHT_FULL_4 = 0xbb,
+    _MID_LEFT_EMPTY = 0xd4,
+    _MID_RIGHT_EMPTY = 0xd6,
+    _MID_LEFT_FULL = 0xd9,
+    _MID_RIGHT_FULL_1 = 0xda,
+    _MID_RIGHT_FULL_2 = 0xdb,
+    _BOTTOM_EMPTY = 0x96,
+    _BOTTOM_LEFT_FULL = 0xd8,
+    _BOTTOM_FULL_1 = 0x9c,
+    _BOTTOM_FULL_2 = 0xdc,
+};
+
+// LAYER OFF --------------------------------------------------------------------------------------
+
+static const char PROGMEM LAY_TOP_LEFT_OFF_1[] = {_TOP_LEFT_EMPTY_1, _TOP_EMPTY, 0};
+static const char PROGMEM LAY_TOP_RIGHT_OFF_1[] = {_TOP_EMPTY, _TOP_RIGHT_EMPTY_1, 0};
+static const char PROGMEM LAY_TOP_LEFT_OFF_2[] = {_TOP_LEFT_EMPTY_2, _TOP_LEFT_EMPTY_3, 0};
+static const char PROGMEM LAY_TOP_RIGHT_OFF_2[] = {_TOP_RIGHT_EMPTY_2, _TOP_RIGHT_EMPTY_3, 0};
+
+static const char PROGMEM LAY_MID_LEFT_OFF_1[] = {_TOP_LEFT_EMPTY_1, _MID_LEFT_EMPTY, 0};
+static const char PROGMEM LAY_MID_RIGHT_OFF_1[] = {_MID_RIGHT_EMPTY, _TOP_RIGHT_EMPTY_1, 0};
+static const char PROGMEM LAY_MID_LEFT_OFF_2[] = {_TOP_LEFT_EMPTY_2, _TOP_LEFT_EMPTY_3, 0};
+static const char PROGMEM LAY_MID_RIGHT_OFF_2[] = {_TOP_RIGHT_EMPTY_2, _TOP_RIGHT_EMPTY_3, 0};
+
+static const char PROGMEM LAY_BOTTOM_LEFT_OFF_1[] = {_TOP_LEFT_EMPTY_1, _MID_LEFT_EMPTY, 0};
+static const char PROGMEM LAY_BOTTOM_RIGHT_OFF_1[] = {_MID_RIGHT_EMPTY, _TOP_RIGHT_EMPTY_1, 0};
+static const char PROGMEM LAY_BOTTOM_LEFT_OFF_2[] = {_TOP_LEFT_EMPTY_2, _BOTTOM_EMPTY, 0};
+static const char PROGMEM LAY_BOTTOM_RIGHT_OFF_2[] = {_BOTTOM_EMPTY, _TOP_RIGHT_EMPTY_3, 0};
+
+// LAYER ON ---------------------------------------------------------------------------------------
+
+static const char PROGMEM LAY_TOP_LEFT_ON_1[] = {_TOP_LEFT_FULL_1, _TOP_LEFT_FULL_3, 0};
+static const char PROGMEM LAY_TOP_RIGHT_ON_1[] = {_TOP_RIGHT_FULL_1, _TOP_RIGHT_FULL_3, 0};
+static const char PROGMEM LAY_TOP_LEFT_ON_2[] = {_TOP_LEFT_FULL_2, _TOP_LEFT_FULL_4, 0};
+static const char PROGMEM LAY_TOP_RIGHT_ON_2[] = {_TOP_RIGHT_FULL_2, _TOP_RIGHT_FULL_4, 0};
+
+static const char PROGMEM LAY_MID_LEFT_ON_1[] = {_TOP_LEFT_FULL_1, _MID_LEFT_FULL, 0};
+static const char PROGMEM LAY_MID_RIGHT_ON_1[] = {_MID_RIGHT_FULL_1, _MID_RIGHT_FULL_2, 0};
+static const char PROGMEM LAY_MID_LEFT_ON_2[] = {_TOP_LEFT_FULL_2, _TOP_LEFT_FULL_4, 0};
+static const char PROGMEM LAY_MID_RIGHT_ON_2[] = {_TOP_RIGHT_FULL_2, _TOP_RIGHT_FULL_4, 0};
+
+static const char PROGMEM LAY_BOTTOM_LEFT_ON_1[] = {_TOP_LEFT_FULL_1, _MID_LEFT_FULL, 0};
+static const char PROGMEM LAY_BOTTOM_RIGHT_ON_1[] = {_MID_RIGHT_FULL_1, _TOP_RIGHT_FULL_1, 0};
+static const char PROGMEM LAY_BOTTOM_LEFT_ON_2[] = {_BOTTOM_LEFT_FULL, _BOTTOM_FULL_1, 0};
+static const char PROGMEM LAY_BOTTOM_RIGHT_ON_2[] = {_BOTTOM_FULL_2, _TOP_RIGHT_FULL_3, 0};
+
+// ------------------------------------------------------------------------------------------------
+
+oled_rotation_t oled_init_kb(oled_rotation_t rotation) {
+    return OLED_ROTATION_270;
+}
+
+void render_space(void) {
+    oled_write_P(PSTR("     "), false);
+}
+
+void render_mod_status_gui_alt(uint8_t modifiers) {
+    static const char PROGMEM gui_off_1[] = {0x85, 0x86, 0};
+    static const char PROGMEM gui_off_2[] = {0xa5, 0xa6, 0};
+    static const char PROGMEM gui_on_1[] = {0x8d, 0x8e, 0};
+    static const char PROGMEM gui_on_2[] = {0xad, 0xae, 0};
+
+    static const char PROGMEM alt_off_1[] = {0x87, 0x88, 0};
+    static const char PROGMEM alt_off_2[] = {0xa7, 0xa8, 0};
+    static const char PROGMEM alt_on_1[] = {0x8f, 0x90, 0};
+    static const char PROGMEM alt_on_2[] = {0xaf, 0xb0, 0};
+
+    // fillers between the modifier icons bleed into the icon frames
+    static const char PROGMEM off_off_1[] = {0xc5, 0};
+    static const char PROGMEM off_off_2[] = {0xc6, 0};
+    static const char PROGMEM on_off_1[] = {0xc7, 0};
+    static const char PROGMEM on_off_2[] = {0xc8, 0};
+    static const char PROGMEM off_on_1[] = {0xc9, 0};
+    static const char PROGMEM off_on_2[] = {0xca, 0};
+    static const char PROGMEM on_on_1[] = {0xcb, 0};
+    static const char PROGMEM on_on_2[] = {0xcc, 0};
+
+    if(modifiers & MOD_MASK_GUI) {
+        oled_write_P(gui_on_1, false);
+    } else {
+        oled_write_P(gui_off_1, false);
+    }
+
+    if ((modifiers & MOD_MASK_GUI) && (modifiers & MOD_MASK_ALT)) {
+        oled_write_P(on_on_1, false);
+    } else if(modifiers & MOD_MASK_GUI) {
+        oled_write_P(on_off_1, false);
+    } else if(modifiers & MOD_MASK_ALT) {
+        oled_write_P(off_on_1, false);
+    } else {
+        oled_write_P(off_off_1, false);
+    }
+
+    if(modifiers & MOD_MASK_ALT) {
+        oled_write_P(alt_on_1, false);
+    } else {
+        oled_write_P(alt_off_1, false);
+    }
+
+    if(modifiers & MOD_MASK_GUI) {
+        oled_write_P(gui_on_2, false);
+    } else {
+        oled_write_P(gui_off_2, false);
+    }
+
+    if (modifiers & MOD_MASK_GUI & MOD_MASK_ALT) {
+        oled_write_P(on_on_2, false);
+    } else if(modifiers & MOD_MASK_GUI) {
+        oled_write_P(on_off_2, false);
+    } else if(modifiers & MOD_MASK_ALT) {
+        oled_write_P(off_on_2, false);
+    } else {
+        oled_write_P(off_off_2, false);
+    }
+
+    if(modifiers & MOD_MASK_ALT) {
+        oled_write_P(alt_on_2, false);
+    } else {
+        oled_write_P(alt_off_2, false);
+    }
+}
+
+void render_mod_status_ctrl_shift(uint8_t modifiers) {
+    static const char PROGMEM ctrl_off_1[] = {0x89, 0x8a, 0};
+    static const char PROGMEM ctrl_off_2[] = {0xa9, 0xaa, 0};
+    static const char PROGMEM ctrl_on_1[] = {0x91, 0x92, 0};
+    static const char PROGMEM ctrl_on_2[] = {0xb1, 0xb2, 0};
+
+    static const char PROGMEM shift_off_1[] = {0x8b, 0x8c, 0};
+    static const char PROGMEM shift_off_2[] = {0xab, 0xac, 0};
+    static const char PROGMEM shift_on_1[] = {0xcd, 0xce, 0};
+    static const char PROGMEM shift_on_2[] = {0xcf, 0xd0, 0};
+
+    // fillers between the modifier icons bleed into the icon frames
+    static const char PROGMEM off_off_1[] = {0xc5, 0};
+    static const char PROGMEM off_off_2[] = {0xc6, 0};
+    static const char PROGMEM on_off_1[] = {0xc7, 0};
+    static const char PROGMEM on_off_2[] = {0xc8, 0};
+    static const char PROGMEM off_on_1[] = {0xc9, 0};
+    static const char PROGMEM off_on_2[] = {0xca, 0};
+    static const char PROGMEM on_on_1[] = {0xcb, 0};
+    static const char PROGMEM on_on_2[] = {0xcc, 0};
+
+    if(modifiers & MOD_MASK_CTRL) {
+        oled_write_P(ctrl_on_1, false);
+    } else {
+        oled_write_P(ctrl_off_1, false);
+    }
+
+    if ((modifiers & MOD_MASK_CTRL) && (modifiers & MOD_MASK_SHIFT)) {
+        oled_write_P(on_on_1, false);
+    } else if(modifiers & MOD_MASK_CTRL) {
+        oled_write_P(on_off_1, false);
+    } else if(modifiers & MOD_MASK_SHIFT) {
+        oled_write_P(off_on_1, false);
+    } else {
+        oled_write_P(off_off_1, false);
+    }
+
+    if(modifiers & MOD_MASK_SHIFT) {
+        oled_write_P(shift_on_1, false);
+    } else {
+        oled_write_P(shift_off_1, false);
+    }
+
+    if(modifiers & MOD_MASK_CTRL) {
+        oled_write_P(ctrl_on_2, false);
+    } else {
+        oled_write_P(ctrl_off_2, false);
+    }
+
+    if (modifiers & MOD_MASK_CTRL & MOD_MASK_SHIFT) {
+        oled_write_P(on_on_2, false);
+    } else if(modifiers & MOD_MASK_CTRL) {
+        oled_write_P(on_off_2, false);
+    } else if(modifiers & MOD_MASK_SHIFT) {
+        oled_write_P(off_on_2, false);
+    } else {
+        oled_write_P(off_off_2, false);
+    }
+
+    if(modifiers & MOD_MASK_SHIFT) {
+        oled_write_P(shift_on_2, false);
+    } else {
+        oled_write_P(shift_off_2, false);
+    }
+}
+
+void render_logo(void) {
+    static const char PROGMEM aurora_logo[] = {
+        0x00, 0x00, 0x00, 0xe0, 0x00, 0xf8, 0xc0, 0xf8, 0xe0, 0xc0, 0xfc, 0x00, 0x7e, 0x18, 0x00, 0x80,
+        0x00, 0x02, 0x80, 0xf0, 0x00, 0xc0, 0x80, 0xf8, 0xc0, 0xe0, 0x70, 0x60, 0x3c, 0x38, 0x3c, 0x1c,
+        0x00, 0x3f, 0x0c, 0x0f, 0x1f, 0x03, 0x07, 0x01, 0xc3, 0x00, 0xe0, 0x80, 0x00, 0xe0, 0x80, 0xf8,
+        0x80, 0xc0, 0xf7, 0xc7, 0x6f, 0x7b, 0x39, 0x30, 0x00, 0x80, 0x00, 0xc0, 0x00, 0xc0, 0xc2, 0xe0,
+        0x00, 0x40, 0x38, 0x30, 0x38, 0x1e, 0x18, 0x1e, 0x0f, 0x0c, 0x07, 0x07, 0x07, 0x03, 0x03, 0x21,
+        0x21, 0x31, 0x30, 0x18, 0x18, 0x1c, 0x08, 0x0c, 0x0e, 0x07, 0x06, 0x07, 0x03, 0xc3, 0x03, 0x01,
+        0x4c, 0xcc, 0xc2, 0xc2, 0x41, 0x49, 0x09, 0x2b, 0x2a, 0x6a, 0x6e, 0x24, 0x24, 0x04, 0x92, 0x92,
+        0xb1, 0xf1, 0xf1, 0xf2, 0xe6, 0xa4, 0xa4, 0x04, 0x04, 0x28, 0x28, 0x28, 0x28, 0x28, 0x28, 0x28
+    };
+    oled_write_raw_P(aurora_logo, sizeof(aurora_logo));
+    oled_set_cursor(0, 4);
+}
+
+void render_logo_text(void) {
+    char buf[5];
+    char wpmbuf[3];
+    memset(buf, '0', 5);
+    char wpm = get_current_wpm();
+    sprintf(wpmbuf, "%d", wpm);
+    sprintf(buf + (5 - strlen(wpmbuf)), "%s", wpmbuf);
+    oled_write(PSTR(buf), false);
+    //oled_write_P(PSTR("TMK 2"), false);
+}
+
+void render_kb_LED_state(void) {
+    // Host Keyboard LED Status
+    led_t led_usb_state = host_keyboard_led_state();
+    oled_write_P(led_usb_state.num_lock ? PSTR("N ") : PSTR("  "), false);
+    oled_write_P(led_usb_state.caps_lock ? PSTR("C ") : PSTR("  "), false);
+    oled_write_P(led_usb_state.scroll_lock ? PSTR("S ") : PSTR("  "), false);
+}
+
+void render_layer_state(void) {
+    oled_write_P(IS_LAYER_ON(_EXT) ? LAY_TOP_LEFT_ON_1 : LAY_TOP_LEFT_OFF_1, false);
+    oled_write_char(_MID_1, false);
+    oled_write_P(IS_LAYER_ON(_FUN) ? LAY_TOP_RIGHT_ON_1 : LAY_TOP_RIGHT_OFF_1, false);
+    oled_write_P(IS_LAYER_ON(_EXT) ? LAY_TOP_LEFT_ON_2 : LAY_TOP_LEFT_OFF_2, false);
+    oled_write_char(_MID_2, false);
+    oled_write_P(IS_LAYER_ON(_FUN) ? LAY_TOP_RIGHT_ON_2 : LAY_TOP_RIGHT_OFF_2, false);
+
+    oled_write_P(IS_LAYER_ON(_SYM) ? LAY_MID_LEFT_ON_1 : LAY_MID_LEFT_OFF_1, false);
+    oled_write_char(_MID_3, false);
+    oled_write_P(IS_LAYER_ON(_NUM) ? LAY_MID_RIGHT_ON_1 : LAY_MID_RIGHT_OFF_1, false);
+    oled_write_P(IS_LAYER_ON(_SYM) ? LAY_MID_LEFT_ON_2 : LAY_MID_LEFT_OFF_2, false);
+    oled_write_char(_MID_2, false);
+    oled_write_P(IS_LAYER_ON(_NUM) ? LAY_MID_RIGHT_ON_2 : LAY_MID_RIGHT_OFF_2, false);
+
+    oled_write_P(IS_LAYER_ON(_FIVE) ? LAY_BOTTOM_LEFT_ON_1 : LAY_BOTTOM_LEFT_OFF_1, false);
+    oled_write_char(_MID_3, false);
+    oled_write_P(IS_LAYER_ON(_SIX) ? LAY_BOTTOM_RIGHT_ON_1 : LAY_BOTTOM_RIGHT_OFF_1, false);
+    oled_write_P(IS_LAYER_ON(_FIVE) ? LAY_BOTTOM_LEFT_ON_2 : LAY_BOTTOM_LEFT_OFF_2, false);
+    oled_write_char(_MID_4, false);
+    oled_write_P(IS_LAYER_ON(_SIX) ? LAY_BOTTOM_RIGHT_ON_2 : LAY_BOTTOM_RIGHT_OFF_2, false);
+}
+
+bool oled_task_kb(void) {
+    if (!oled_task_user()) {
+        return false;
+    }
+    if (is_keyboard_master()) {
+        // Renders the current keyboard state (layers and mods)
+        render_logo();
+        render_logo_text();
+        render_layer_state();
+        render_mod_status_gui_alt(get_mods()|get_oneshot_mods());
+        render_mod_status_ctrl_shift(get_mods()|get_oneshot_mods());
+        render_kb_LED_state();
+    } else {
+        // clang-format off
+        static const char PROGMEM aurora_art[] = {
+            0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x1c, 0x08, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x40,
+            0xe0, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x80, 0xc0, 0x80,
+            0xc0, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xe0, 0x00, 0x80, 0x00, 0xf0, 0x00, 0x00, 0xc0,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00,
+            0x81, 0x00, 0xc0, 0x00, 0xfe, 0x00, 0xfc, 0x00, 0xff, 0x20, 0xff, 0xf0, 0x0f, 0xf0, 0x00, 0xff,
+            0x00, 0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x90, 0x00, 0xf8, 0x00, 0x00, 0xf8,
+            0xff, 0x10, 0xff, 0x84, 0xff, 0x60, 0xff, 0x36, 0xff, 0x0f, 0xff, 0x3f, 0x00, 0x5f, 0x00, 0x05,
+            0x80, 0x00, 0x80, 0x00, 0xc0, 0x38, 0x00, 0xec, 0xf0, 0x00, 0xfb, 0x80, 0xff, 0xf0, 0xff, 0xef,
+            0xff, 0xe8, 0xff, 0x03, 0xff, 0x0c, 0xff, 0x00, 0xff, 0x00, 0x03, 0x00, 0x00, 0xf8, 0x00, 0x80,
+            0xff, 0x20, 0xff, 0xd0, 0xff, 0xe0, 0xfe, 0xf8, 0xff, 0xfc, 0xff, 0xff, 0x0f, 0xff, 0x01, 0x3f,
+            0xff, 0x00, 0x0f, 0x00, 0x01, 0x00, 0x03, 0x00, 0xfe, 0x80, 0xfe, 0x00, 0xc0, 0xff, 0xc4, 0xfb,
+            0xff, 0xfe, 0xff, 0xff, 0xff, 0x3f, 0xff, 0xff, 0x07, 0xff, 0x03, 0x3f, 0x00, 0x0f, 0xc0, 0x00,
+            0x00, 0x00, 0xb8, 0x00, 0xff, 0x40, 0xbe, 0xf0, 0xff, 0xf1, 0xff, 0xfe, 0xff, 0xff, 0xff, 0xff,
+            0x1f, 0xff, 0x67, 0x00, 0xef, 0x00, 0x1f, 0x00, 0x00, 0x07, 0x00, 0x00, 0xe0, 0x00, 0xff, 0xf0,
+            0xff, 0x88, 0xff, 0xc4, 0xff, 0xf8, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x8f, 0x7f, 0x0f, 0xff,
+            0x00, 0x07, 0xfe, 0x00, 0x07, 0x00, 0x00, 0x00, 0x00, 0xf8, 0x00, 0xc0, 0x3f, 0xf8, 0xe7, 0xff,
+            0xff, 0xff, 0xff, 0x1f, 0xff, 0xff, 0x1f, 0x3f, 0x01, 0xff, 0x0b, 0x00, 0xff, 0x00, 0x00, 0x05,
+            0x00, 0x00, 0x00, 0xe0, 0x00, 0xf8, 0x60, 0x80, 0xfe, 0xe3, 0xfc, 0xff, 0x1e, 0xff, 0xff, 0x23,
+            0xff, 0x09, 0xff, 0x20, 0x00, 0x3f, 0x02, 0x00, 0x00, 0x0f, 0x00, 0x40, 0x00, 0xc0, 0x00, 0xfc,
+            0xe0, 0xfc, 0xf0, 0xff, 0xff, 0x7f, 0xfc, 0xff, 0x0f, 0xff, 0x07, 0x1f, 0x00, 0x01, 0x0f, 0x00,
+            0x0f, 0x00, 0x81, 0x70, 0x0c, 0xf0, 0x80, 0x00, 0x00, 0xe4, 0xf8, 0xe6, 0x70, 0x3f, 0xcf, 0xff,
+            0x1f, 0xff, 0x48, 0xff, 0x0f, 0x00, 0x07, 0x00, 0x00, 0x43, 0x60, 0xf8, 0xf0, 0xfe, 0x38, 0xfe,
+            0x00, 0xfc, 0x03, 0x00, 0xc8, 0x72, 0xcf, 0xfc, 0x00, 0x03, 0x0f, 0x01, 0xe0, 0x1c, 0xe0, 0x03,
+            0x00, 0x0f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf8, 0x06, 0xf9, 0x00, 0x03, 0x00, 0x07,
+            0xff, 0x00, 0x10, 0x12, 0xc9, 0xf0, 0xcf, 0xb4, 0x7f, 0x80, 0xe0, 0x1e, 0x01, 0x40, 0x65, 0x5e,
+            0xe0, 0x00, 0x00, 0xf0, 0x0c, 0xf0, 0x00, 0x80, 0x7e, 0x01, 0x80, 0x93, 0xfc, 0xc0, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x89, 0x18, 0x2c, 0x46, 0x00, 0x07, 0x21, 0x10, 0x10, 0x80, 0x09, 0x13,
+            0x31, 0xbf, 0xff, 0x00, 0x08, 0x1a, 0xf7, 0x0f, 0x00, 0x00, 0x44, 0x45, 0x34, 0xbf, 0xb8, 0x00,
+            0x10, 0xf0, 0x08, 0xf4, 0x18, 0x11, 0xfc, 0x18, 0xfb, 0x0e, 0x10, 0xf8, 0x04, 0xf8, 0x10, 0x20,
+            0x18, 0x09, 0xff, 0x0c, 0xea, 0x1f, 0x28, 0x60, 0x30, 0xf8, 0x20, 0xc0, 0x42, 0x33, 0x21, 0x00
+        };
+        // clang-format on
+        oled_write_raw_P(aurora_art, sizeof(aurora_art));
+    }
+    return false;
+}
+#endif
+
+#ifdef CAPS_WORD_ENABLE
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+        case RALT(KC_W):
+        case RALT(KC_P):
+        case RALT(KC_Q):
+        case KC_MINS:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+}
+#endif
